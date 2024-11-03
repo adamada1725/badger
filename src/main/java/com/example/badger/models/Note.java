61fab592bbab3771989976ce625eaae68f29a8d3
adamada1725 <adamada1725@gmail.com>
2024-11-04 02:07:22 +0300

    build(database): add database config

---
+++ src/main/java/com/example/badger/models/Note.java
@@ -0,0 +1,46 @@
+package com.example.badger.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "note")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = true, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 2048)
+    private String content;
+
+    public Note(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
