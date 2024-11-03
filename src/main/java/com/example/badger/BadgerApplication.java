692c694ecf895ec36bef62d47068e75ee7adc625
adamada1725 <adamada1725@gmail.com>
2024-11-04 00:57:20 +0300

    init

---
+++ src/main/java/com/example/badger/BadgerApplication.java
@@ -0,0 +1,13 @@
+package com.example.badger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadgerApplication.class, args);
+	}
+
+}
