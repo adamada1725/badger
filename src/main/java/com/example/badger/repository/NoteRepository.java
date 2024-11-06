cfad8766e63b8fb0ccef09b789b08eda8f850608
adamada1725 <adamada1725@gmail.com>
2024-11-07 00:04:13 +0300

    build(database): add database config
    This reverts commit 61fab592bbab3771989976ce625eaae68f29a8d3.

--- src/main/java/com/example/badger/repository/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.badger.repository;
-
-import com.example.badger.models.Note;
-import org.springframework.data.repository.CrudRepository;
-
-public interface NoteRepository extends CrudRepository<Note, Long> {
-
-}
