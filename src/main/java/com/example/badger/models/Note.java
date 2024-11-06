d394bb7d1cb2ac5e753a9e7616b8ec0eef0ccc93
adamada1725 <adamada1725@gmail.com>
2024-11-07 00:55:19 +0300

    fix(database): alter table note generationtype.identity

--- src/main/java/com/example/badger/models/Note.java
+++ src/main/java/com/example/badger/models/Note.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Note {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = true, length = 100)
